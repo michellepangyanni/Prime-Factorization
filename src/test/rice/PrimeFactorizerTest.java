511f3f0c1cce65643d588cd003bb3dc52701e5c7
Michelle <yp29@rice.edu>
2022-09-07 12:25:29 -0500

    code modified

--- src/test/rice/PrimeFactorizerTest.java
+++ src/test/rice/PrimeFactorizerTest.java
@@ -25,26 +25,34 @@ public class PrimeFactorizerTest {
         int[] actual = factorizer100.computePrimeFactorization(-1);
         assertNull(actual);
     }
-
+    /**
+     * Tests that attempting factorization of a 0 returns null.
+     */
     @Test
     void testFactorize0() {
         int[] actual = factorizer100.computePrimeFactorization(0);
         assertNull(actual);
     }
-
+    /**
+     * Tests that attempting factorization of a 1 returns null.
+     */
     @Test
     void testFactorize1() {
         int[] actual = factorizer100.computePrimeFactorization(1);
         assertNull(actual);
     }
-
+    /**
+     * Tests with inputting a prime (>1) that can be factorized
+     */
     @Test
     void testFactorize2() {
         int[] actual = factorizer100.computePrimeFactorization(2);
         int[] expected = new int[]{2};
         assertArrayEquals(expected, actual);
     }
-
+    /**
+     * Test with input's factorization includes more than one distinct prime
+     */
     @Test
     void testFactorize3() {
         int[] actual = factorizer100.computePrimeFactorization(3);
@@ -52,13 +60,18 @@ public class PrimeFactorizerTest {
         assertArrayEquals(expected, actual);
     }
 
+    /**
+     * Test with the input is a composite (non-prime) that can be factorized
+     */
     @Test
     void testFactorize10() {
         int[] actual = factorizer100.computePrimeFactorization(10);
         int[] expected = new int[]{2, 5};
         assertArrayEquals(expected, actual);
     }
-
+    /**
+     * Test with input's factorization includes repeats
+     */
     @Test
     void testFactorize8() {
         int[] actual = factorizer100.computePrimeFactorization(8);
@@ -66,18 +79,27 @@ public class PrimeFactorizerTest {
         assertArrayEquals(expected, actual);
     }
 
+    /**
+     * The input is a prime that is too large to be factorized
+     */
     @Test
     void testFactorize101() {
         int[] actual = factorizer100.computePrimeFactorization(101);
         assertNull(actual);
     }
 
+    /**
+     * The input is a composite that is too large to be factorized
+     */
     @Test
     void testFactorize102() {
         int[] actual = factorizer100.computePrimeFactorization(102);
         assertNull(actual);
     }
 
+    /**
+     * The input is a prime (>1) that can be factorized
+     */
     @Test
     void testFactorize97() {
         int[] actual = factorizer100.computePrimeFactorization(97);
@@ -85,6 +107,9 @@ public class PrimeFactorizerTest {
         assertArrayEquals(expected, actual);
     }
 
+    /**
+     * The input is the upper bound of PrimeFactorizer
+     */
     @Test
     void testFactorize100() {
         int[] actual = factorizer100.computePrimeFactorization(100);
@@ -92,7 +117,14 @@ public class PrimeFactorizerTest {
         assertArrayEquals(expected, actual);
     }
 
+    /**
+     * A prime factorizer with an upper bound of 20000
+     */
     private static final PrimeFactorizer factorizer20000 = new PrimeFactorizer(20000);
+
+    /**
+     * The input is a very large (> 10000) prime that can be factorized
+     */
     @Test
     void testFactorize19997() {
         int[] actual = factorizer20000.computePrimeFactorization(19997);
@@ -100,6 +132,9 @@ public class PrimeFactorizerTest {
         assertArrayEquals(expected, actual);
     }
 
+    /**
+     * The input is a very large (> 10000) composite that can factorized
+     */
     @Test
     void testFactorize20000() {
         int[] actual = factorizer20000.computePrimeFactorization(20000);
